cef11861509506a0747d51bc6476bf2093007d71
Sebastian Ramirez <u20201c031@upc.edu.pe>
2023-11-19 08:12:49 -0500

    feat(add .env and upd server.js)

--- server.js
+++ server.js
@@ -1,45 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const config = require('./config/config.json')[env];
-const { Sequelize } = require('sequelize');
 
-const touristRoutes = require('./routes/tourist_routes');
+
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+
 
 const app = express();
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  dialect: config.dialect
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql', 
 });
 
 // Middleware
 app.use(bodyParser.json());
 
 // Rutas
-
+const touristRoutes = require('./routes/tourist_routes');
 app.use('/', touristRoutes);
 
 
 
 
-/*// Sincronizar el modelo con la base de datos
+// Sincronizar el modelo con la base de datos
 sequelize.sync()
   .then(() => {
     // Iniciar el servidor después de sincronizar el modelo*/
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send({ message: 'Something went wrong!' });
-});
+
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log('Conexión a la base de datos establecida y modelo sincronizado.');
   console.log(`Servidor corriendo en ${PORT}`);
 });
-  /*})
+  })
   .catch(error => {
     console.error('No se pudo conectar a la base de datos o sincronizar el modelo:', error);
-  });*/
+  });
 
 module.exports = app;
