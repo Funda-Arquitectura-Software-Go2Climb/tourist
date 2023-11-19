993516ae787619ab3ea83478f2c8c5aa9c223ae0
Sebastian Ramirez <u20201c031@upc.edu.pe>
2023-11-19 07:18:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ app.use(bodyParser.json());
 const touristRoutes = require('./routes/tourist_routes');
 app.use('/', touristRoutes);
 
-// Sincronizar el modelo con la base de datos
+/*// Sincronizar el modelo con la base de datos
 sequelize.sync()
   .then(() => {
-    // Iniciar el servidor después de sincronizar el modelo
+    // Iniciar el servidor después de sincronizar el modelo*/
     app.listen(PORT, () => {
       console.log('Conexión a la base de datos establecida y modelo sincronizado.');
       console.log(`Servidor corriendo en http://localhost:${PORT}`);
     });
-  })
+  /*})
   .catch(error => {
     console.error('No se pudo conectar a la base de datos o sincronizar el modelo:', error);
-  });
+  });*/
