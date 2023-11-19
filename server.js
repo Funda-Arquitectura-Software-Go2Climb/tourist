10a1e62a22ef3e53a415ed52e79db0b215b07427
Sebastian Ramirez <u20201c031@upc.edu.pe>
2023-11-19 07:29:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Sequelize } = require('sequelize');
-const app = express();
-const PORT = 3002;
+//const config = require('./config/config.json')[env];
+//const { Sequelize } = require('sequelize');
 
-const env = process.env.NODE_ENV || 'development';
-const config = require('./config/config.json')[env];
-const sequelize = new Sequelize(config.database, config.username, config.password, {
+const touristRoutes = require('./routes/tourist_routes');
+
+const app = express();
+
+/*const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
   dialect: config.dialect
-});
+});*/
 
 // Middleware
 app.use(bodyParser.json());
 
 // Rutas
-const touristRoutes = require('./routes/tourist_routes');
+
 app.use('/', touristRoutes);
+
+const PORT = process.env.PORT || 3002;
+
 
 /*// Sincronizar el modelo con la base de datos
 sequelize.sync()
@@ -24,9 +28,11 @@ sequelize.sync()
     // Iniciar el servidor después de sincronizar el modelo*/
     app.listen(PORT, () => {
       console.log('Conexión a la base de datos establecida y modelo sincronizado.');
-      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+      console.log(`Servidor corriendo en ${PORT}`);
     });
   /*})
   .catch(error => {
     console.error('No se pudo conectar a la base de datos o sincronizar el modelo:', error);
   });*/
+
+  module.exports = app;
